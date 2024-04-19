5d9bef307873d26b1cbc913b3bcd78749dee7098
huynhh21 <huynhh21@gmail.com>
2024-04-18 21:06:48 -0500

    Create FallbackController.cs

---
+++ Controllers/FallbackController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.IO;
+
+namespace BackEnd.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), MediaTypeNames.Text.Html);
+        }
+    }
+}
